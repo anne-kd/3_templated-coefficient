252103fcdfc9acf97e584b3c50349e9774a9e0e4
Anne-Kathrin <annekdrotleff@gmail.com>
2020-03-28 12:29:31 +0100

    update y mousemovement

--- script.js
+++ script.js
@@ -14,19 +14,22 @@ var input = {
 //Reichweite festlegen (wenn ich später das Mousemovement nur innerhalb des Wrappers ansprechen will)
 // Reichweite = Ende - Start
 input.mouseX.range = input.mouseX.end - input.mouseX.start;
+input.mouseY.range = input.mouseY.end - input.mouseY.start;
 //console.log(input.mouseX.range); 1680
 
 //Wollen nur zahlen von 0-1
 //Zahl = (aktuelle Position - Start Position): gesamte Länge/Höhe /Reichweite
 //input.mouseX.part = (input.mouseX.current - input.mouseX.start)/ input.mouseX.range;
+//input.mouseY.part = (input.mouseY.current - input.mouseY.start)/ input.mouseY.range;
 
 
 var MouseMovement = function(e){
     input.mouseX.current = e.clientX;
     input.mouseX.part = (input.mouseX.current - input.mouseX.start)/ input.mouseX.range;
-    console.log('x', input.mouseX.part);
+    //console.log('x', input.mouseX.part);
     input.mouseY.current = e.clientY;
-    console.log('y', input.mouseY.current);
+    input.mouseY.part = (input.mouseY.current - input.mouseY.start)/ input.mouseY.range;
+    //console.log('y', input.mouseY.current);
 }
 
 window.addEventListener('mousemove',  MouseMovement)
