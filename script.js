5d141724dc1db7dd692141b8f768b876dc0a1545
Anne-Kathrin <annekdrotleff@gmail.com>
2020-03-28 11:32:47 +0100

    capture mousemove
    ++ fixed spelling mistake in script.js to link script

---
+++ script.js
@@ -0,0 +1,12 @@
+var input = {
+    mouseX: {
+        start:0,
+        end: window.innerWidth,
+        current: 0
+    }
+}
+
+window.addEventListener('mousemove', 
+    function(){
+        this.console.log('working');
+    })
