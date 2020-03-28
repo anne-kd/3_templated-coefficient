2bc5001bd3789872819001a47575100bd2c286dc
Anne-Kathrin <annekdrotleff@gmail.com>
2020-03-28 12:28:06 +0100

    defined range and part*
    *part = we only want numbers between 0 and 1

--- script.js
+++ script.js
@@ -11,12 +11,20 @@ var input = {
     }
 }
 
+//Reichweite festlegen (wenn ich später das Mousemovement nur innerhalb des Wrappers ansprechen will)
+// Reichweite = Ende - Start
+input.mouseX.range = input.mouseX.end - input.mouseX.start;
+//console.log(input.mouseX.range); 1680
 
+//Wollen nur zahlen von 0-1
+//Zahl = (aktuelle Position - Start Position): gesamte Länge/Höhe /Reichweite
+//input.mouseX.part = (input.mouseX.current - input.mouseX.start)/ input.mouseX.range;
 
 
 var MouseMovement = function(e){
     input.mouseX.current = e.clientX;
-    console.log('x', input.mouseX.current);
+    input.mouseX.part = (input.mouseX.current - input.mouseX.start)/ input.mouseX.range;
+    console.log('x', input.mouseX.part);
     input.mouseY.current = e.clientY;
     console.log('y', input.mouseY.current);
 }
